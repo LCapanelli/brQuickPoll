1db1e7f9405cd21762b2fcee91eb9638bd38e314
Lucas <Lucas@LCS-Y508-NT>
2018-11-28 19:28:43 -0200

    1st commit to repo.

---
+++ src/main/java/com/brquickpoll/brquickpoll/BrquickpollApplication.java
@@ -0,0 +1,12 @@
+package com.brquickpoll.brquickpoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrquickpollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrquickpollApplication.class, args);
+	}
+}
---
+++ src/test/java/com/brquickpoll/brquickpoll/BrquickpollApplicationTests.java
@@ -0,0 +1,16 @@
+package com.brquickpoll.brquickpoll;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BrquickpollApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
