ea0bb8b6ca9a9aa979ea095df5af7e8ee5e7462f
Lucas <Lucas@LCS-Y508-NT>
2018-12-02 19:50:25 -0200

    Adding vote controller

---
+++ src/main/java/com/brquickpoll/controller/VoteController.java
@@ -0,0 +1,44 @@
+package com.brquickpoll.controller;
+
+import java.net.URI;
+import java.util.Optional;
+
+import javax.inject.Inject;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.brquickpoll.domain.Vote;
+import com.brquickpoll.repository.VoteRepository;;
+
+@RestController
+public class VoteController {
+
+	@Inject
+	private VoteRepository voteRepository; 
+	
+	@RequestMapping(value="/polls/{pollId}/votes", method=RequestMethod.POST)
+	public ResponseEntity<?> createVote(@PathVariable Long pollId, @RequestBody Vote vote){
+		vote = voteRepository.save(vote); 
+		
+		//Setting Headers for the newly create resource
+		HttpHeaders responseHeaders = new HttpHeaders(); 
+		responseHeaders.setLocation(ServletUriComponentsBuilder
+				.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(vote.getId())
+				.toUri());
+		
+		return new ResponseEntity<> (null, responseHeaders, HttpStatus.CREATED); 
+	}
+}
