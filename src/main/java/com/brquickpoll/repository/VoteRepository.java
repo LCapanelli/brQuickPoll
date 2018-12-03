a489c05bd56c241d84c44ce2788087dae667dec5
Lucas <Lucas@LCS-Y508-NT>
2018-12-02 22:37:09 -0200

    Vote Controller creation and Repo adjustment

--- src/main/java/com/brquickpoll/repository/VoteRepository.java
+++ src/main/java/com/brquickpoll/repository/VoteRepository.java
@@ -1,8 +1,13 @@
 package com.brquickpoll.repository;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 import com.brquickpoll.domain.Vote;
 
 public interface VoteRepository extends CrudRepository<Vote, Long>{
 
+	@Query(value="SELECT v.* FROM Option o, Vote v "
+			+ "WHERE o.POLL_ID = ?1 AND v.OPTION_ID = o.OPTION_ID", 
+			nativeQuery=true)
+	public Iterable<Vote> findByPoll(Long pollId);
 }
