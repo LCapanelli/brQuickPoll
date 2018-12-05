e77613adb797182a17b7bbb43d2e6407af593ac4
Lucas <Lucas@LCS-Y508-NT>
2018-12-04 22:27:25 -0200

    New DTOs and ComputeController

--- src/main/java/com/brquickpoll/controller/VoteController.java
+++ src/main/java/com/brquickpoll/controller/VoteController.java
@@ -13,9 +13,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.brquickpoll.domain.Vote;
