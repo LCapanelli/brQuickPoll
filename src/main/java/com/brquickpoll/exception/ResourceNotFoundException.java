703e3a5f1595b52668ec8575a0b9a18e8ac397b2
Lucas Capanelli <lucas.capanelli.es@gmail.com>
2018-12-05 23:10:37 -0200

    After merge, new Exception Package

---
+++ src/main/java/com/brquickpoll/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.brquickpoll.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+public class ResourceNotFoundException extends RuntimeException{
+
+}
