6ddc29bfd8ceac8ddc6d2bc169c6ade69c7dfc00
Lucas Capanelli <lucas.capanelli.es@gmail.com>
2018-12-08 16:15:15 -0200

    Improved validation error

--- src/main/java/com/brquickpoll/handler/RestExceptionHandler.java
+++ src/main/java/com/brquickpoll/handler/RestExceptionHandler.java
@@ -4,13 +4,19 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
 import com.brquickpoll.dto.error.ErrorDetail;
 import com.brquickpoll.dto.error.ValidationError;
 import com.brquickpoll.exception.ResourceNotFoundException;
@@ -18,8 +24,52 @@ import com.brquickpoll.exception.ResourceNotFoundException;
 @ControllerAdvice
 public class RestExceptionHandler {
 
+	@Inject 
+	private MessageSource messageSource; 
+	
 	@ExceptionHandler(MethodArgumentNotValidException.class)
-	public ResponseEntity<?> handleValidationError(MethodArgumentNotValidException methArgNotValid, HttpServletRequest req){
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	public @ResponseBody ErrorDetail handleValidationError(MethodArgumentNotValidException methArgNotValid, HttpServletRequest req){
+		
+		ErrorDetail errorDetail = new ErrorDetail();
+		//Title: brief title for the error 
+		errorDetail.setTitle("Validation Failed");
+		//Status: HTTP status code 
+		errorDetail.setStatus(HttpStatus.BAD_REQUEST.value());
+		//Detail: Short description of the error to an end user
+		errorDetail.setDetail("Input Validatoin Failed");
+		//Time Stamp: time in milliseconds when the error has occurred
+		errorDetail.setTimeStamp(new Date().getTime());
+		//devMessage: tech info for devs	
+		errorDetail.setDevMessage(methArgNotValid.getClass().getName());
+		//URI error
+		String requestPath = (String) req.getAttribute("javax.servlet.error.request_uri");
+			if (requestPath == null) {
+				requestPath = req.getRequestURI();
+			}
+			
+		//Creating ValidationError instances
+		List<FieldError> fieldErrors = methArgNotValid.getBindingResult().getFieldErrors();
+			for(FieldError f : fieldErrors) {
+				List<ValidationError> validationErrorList = errorDetail.getErrors().get(f.getField());
+				if (validationErrorList == null) {
+					validationErrorList = new ArrayList<ValidationError>();
+					errorDetail.getErrors().put(f.getField(),
+							validationErrorList);
+				}
+			ValidationError validationError = new ValidationError();
+			validationError.setCode(f.getCode());
+			validationError.setMessage(messageSource.getMessage(f, null));
+			validationErrorList.add(validationError);
+			}
+			
+		return errorDetail;	
+	}
+	
+	
+	//Replaced by the method above
+	@Deprecated
+	public ResponseEntity<?> oldHandleValidationError(MethodArgumentNotValidException methArgNotValid, HttpServletRequest req){
 		
 		ErrorDetail errorDetail = new ErrorDetail();
 		//Title: brief title for the error 
