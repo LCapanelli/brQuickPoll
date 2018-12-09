155d9e38c32c9cd807ed9bffd9a68f39096efe5c
Lucas Capanelli <lucas.capanelli.es@gmail.com>
2018-12-09 16:54:44 -0200

    1s commit - adding Swagger dependencies

--- src/main/java/com/brquickpoll/BrquickpollApplication.java
+++ src/main/java/com/brquickpoll/BrquickpollApplication.java
@@ -2,8 +2,10 @@ package com.brquickpoll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import com.mangofactory.swagger.plugin.EnableSwagger;
 
 @SpringBootApplication
+@EnableSwagger
 public class BrquickpollApplication {
 
 	public static void main(String[] args) {
