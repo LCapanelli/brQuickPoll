ba7afeeffc296641eef18a09dd1eea1552e4c674
Lucas Capanelli <lucas.capanelli.es@gmail.com>
2018-12-17 21:26:31 -0200

    get all Polls method

--- src/main/java/com/brquickpoll/client/BrQuickPollClient.java
+++ src/main/java/com/brquickpoll/client/BrQuickPollClient.java
@@ -3,6 +3,12 @@ package com.brquickpoll.client;
 import org.springframework.web.client.RestTemplate;
 import com.brquickpoll.domain.Poll;
 
+import java.util.List;
+
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpMethod;
+
 public class BrQuickPollClient {
 
 	private static final String BRQUICK_POLL_URI_V1 = "http://localhost:8080/v1/polls";
@@ -10,5 +16,14 @@ public class BrQuickPollClient {
 	
 	public Poll getPollById(Long pollId) {
 		return restTemplate.getForObject(BRQUICK_POLL_URI_V1 + "/{pollId}", Poll.class, pollId);
+	}
+	
+	public List<Poll> getAllPolls() {
+		ParameterizedTypeReference<List<Poll>> responseType = new ParameterizedTypeReference <List<Poll>>() {};
+		ResponseEntity<List<Poll>> responseEntity = restTemplate.exchange(BRQUICK_POLL_URI_V1, HttpMethod.GET, 
+				null, responseType);
+		List<Poll> allPolls = responseEntity.getBody();
+		
+		return allPolls;
 	}
 }
