5dcab7e716abbdc690fe5a042b05804214c6082d
Lucas Capanelli <lucas.capanelli.es@gmail.com>
2019-01-06 12:07:40 -0200

    V3 Auth client

---
+++ src/main/java/com/brquickpoll/client/BrQuickPollClientV3BasicAuth.java
@@ -0,0 +1,23 @@
+package com.brquickpoll.client;
+
+import org.apache.tomcat.util.codec.binary.Base64;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+
+public class BrQuickPollClientV3BasicAuth {
+
+	
+	//Basic Authentication for V3 client
+	private HttpHeaders getAuthenticationHeader(String username, String password) {
+			
+		String credentials = username + ":" + password;
+		byte[] base64CredentialData = Base64.encodeBase64(credentials.getBytes());
+			
+		HttpHeaders headers = new HttpHeaders();
+		headers.set("Authorization", "Basic " + new String(base64CredentialData));
+		
+		return headers;
+	}
+}
